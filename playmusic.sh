6217db240083b568de73a54050a30883be52ccde
Ashutosh Chaudhary <216.ashutosh@gmail.com>
2016-05-22 22:53:51 +0530

    changed cd

--- playmusic.sh
+++ playmusic.sh
@@ -4,7 +4,4 @@ echo Enter music name ":"
 
 read x
 
-cd ~
-#$(python search.py -b -q "$x")
-
 youtube-dl -f worst -o - $(python search.py -b -q "$x") | ffmpeg -i - -ab 192k -vn -acodec libmp3lame -f mp3 - |  mplayer -
